4cbe10d041a274f01a3d788ba0da096ec13f2588
godswillokokon <godswillokokon3@gmail.com>
2019-09-27 16:08:57 +0100

    cleaning profile

--- app/screens/profile/index.js
+++ app/screens/profile/index.js
@@ -3,13 +3,10 @@ import { View, StyleSheet, Dimensions, Text, TouchableOpacity } from "react-nati
 import { connect } from "react-redux";
 import { Container, Content, Thumbnail, Form, Item, Label, Input } from "native-base";
 import { GetUserData } from "../../redux/actions/UserActions";
-import Session from "../../utils/Session";
 import { FontAwesome } from "@expo/vector-icons";
 import FooterComponet from "../footer";
 import Head from "../header";
-import {
-  ProgressChart,
-} from 'react-native-chart-kit'
+import { ProgressChart } from 'react-native-chart-kit'
 
 class Profile extends Component {
   constructor(props) {
@@ -21,34 +18,24 @@ class Profile extends Component {
       faculty: "",
       phoneNumber: "",
     };
-
   }
   render() {
     const device_width = Dimensions.get("window").width;
-    const device_height = Dimensions.get("window").height;
     const { user } = this.props;
-    // console.log("user pro", user);
-    // console.log("user pro", user.user);
-    let ass = 0.6;
+    let ass = 0.4;
     const data = {
-      labels: ['Score', 'Quiz', 'IQ'], // optional
+      labels: ['Score', 'Quiz', 'IQ'],
       data: [ass, ass, ass]
     }
     const chartConfig = {
-      // backgroundGradientFrom: '#1e2326',
       backgroundGradientTo: '#1e2326',
       color: (opacity = 2) => `rgba(103, 113, 228, ${opacity})`,
-      // color: '#6771e4',
-      strokeWidth: 3, // optional, default 3
-
+      strokeWidth: 3,
     }
-    const name = `${user.user.name}`;
-    // console.log("usererer", user.user);
-    return (
 
+    return (
       <Container style={styles.container}>
         <Head navigation={this.props.navigation} />
-
         <Content>
           <View style={styles.image}>
             <Thumbnail
@@ -57,32 +44,30 @@ class Profile extends Component {
             />
           </View>
           <View>
-            <Form style={{ margin: 10 }}>
-              <Item floatingLabel>
-                <Label>Name</Label>
+            <Form style={{ margin: 15 }}>
+              <Item>
+                <Label>Name:</Label>
                 <Input disabled value={user.user.name} style={styles.info} />
               </Item>
-              <Item floatingLabel >
-                <Label> Faculty</Label>
+              <Item>
+                <Label>Faculty:</Label>
                 <Input disabled value={user.user.faculty} style={styles.info} />
               </Item>
-              <Item floatingLabel>
-                <Label>Mobile</Label>
+              <Item>
+                <Label>Mobile:</Label>
                 <Input disabled value={user.user.phoneNumber} style={styles.info} />
               </Item>
-              <Item floatingLabel >
-                <Label>Age</Label>
+              <Item>
+                <Label>Age:</Label>
                 <Input disabled value={user.user.age} style={styles.info} />
               </Item>
-              <Item floatingLabel >
-                <Label>Sex</Label>
+              <Item>
+                <Label>Sex:</Label>
                 <Input disabled value={user.user.sex} style={styles.info} />
               </Item>
-
             </Form>
             <TouchableOpacity
               style={styles.edit}
-
             >
               <Text style={styles.editText} onPress={() => this.props.navigation.navigate("Assessment")}>Self Assessment <FontAwesome name="history" color={"#1e2326"} size={20} /></Text>
             </TouchableOpacity>
@@ -97,8 +82,6 @@ class Profile extends Component {
         <FooterComponet name="profile" props={this.props} />
       </Container>
     );
-
-
   }
 }
 const mapStateToProps = state => {
@@ -107,9 +90,7 @@ const mapStateToProps = state => {
 };
 
 const mapDispatchToProps = dispatch => ({
-
   getUser: token => dispatch(GetUserData(token)),
-
 });
 export default connect(
   mapStateToProps,
